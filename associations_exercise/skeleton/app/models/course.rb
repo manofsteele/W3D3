8b71f02f2917c625033d48ba6d691052326a1d23
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-07-04 18:10:21 -0700

    Retry visit commit

--- associations_exercise/skeleton/app/models/course.rb
+++ associations_exercise/skeleton/app/models/course.rb
@@ -11,10 +11,22 @@ class Course < ApplicationRecord
   has_many :prerequisites, 
     primary_key: :prereq_id,
     foreign_key: :id,
-    class_name: :Course
-    
-  has_one :instructor,
-    primary_key: :instructor_id,
-    foreign_key: :id,
+    class_name: :Course,
+    optional: true
+  # 
+  # 
+  # The first attempt below is not preferred style, and is 
+  # considered an improper use of the has-many/has-one construction.
+  # Nearly always, belongs_to is preferred; think of objects 
+  # as real objects in the world.
+  
+  # has_one :instructor,
+  #   primary_key: :instructor_id,
+  #   foreign_key: :id,
+  #   class_name: :User
+  belongs_to :instructor,
+    primary_key: :id, 
+    foreign_key: :instructor_id,
     class_name: :User
+
 end
